03dc9f38a4c121720cdca86e8585bd76d3c23444
Alireza MASOOM <alireza.masoom@polymtl.ca>
2020-09-14 12:57:39 -0400

    Creatio

---
+++ include/IIDM/extensions/remoteMeasurements/version.h
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2016-2019, RTE (http://www.rte-france.com)
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Libiidm, a library to model IIDM networks and allows importing and exporting them to files.
+//
+
+/**
+ * @file Version.h
+ * @brief defines version constants
+ */
+
+#ifndef LIBIIDM_EXTENSIONS_REMOTEMEASUREMENTS_GUARD_VERSION_H
+#define LIBIIDM_EXTENSIONS_REMOTEMEASUREMENTS_GUARD_VERSION_H
+
+//cmake generated constants.
+#define IIDM_EXT_REMOTEMEASUREMENTS_VERSION_MAJOR 0
+#define IIDM_EXT_REMOTEMEASUREMENTS_VERSION_MINOR 2
+#define IIDM_EXT_REMOTEMEASUREMENTS_VERSION_PATCH 8
+
+#define IIDM_EXT_REMOTEMEASUREMENTS_VERSION_STRING "0.2.8"
+
+#endif
---
+++ include/xml/sax/formatter/FormatterVersion.h
@@ -0,0 +1,38 @@
+//
+// Copyright (c) 2015-2019, RTE (http://www.rte-france.com)
+// All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Libxml, a library to handle XML files parsing.
+//
+
+/**
+ * @file  FormatterVersion.h
+ *
+ * @brief Define the formatter library version
+ *
+ */
+
+#ifndef XML_SAX_FORMATTER_GUARD_VERSION_H
+#define XML_SAX_FORMATTER_GUARD_VERSION_H
+
+namespace xml {
+namespace sax {
+namespace formatter {
+
+//cmake generated constants.
+
+#define XML_SAX_FORMATTER_VERSION_MAJOR 0
+#define XML_SAX_FORMATTER_VERSION_MINOR 2
+#define XML_SAX_FORMATTER_VERSION_PATCH 4
+
+#define XML_SAX_FORMATTER_VERSION "0.2.4"
+
+} // end of namespace xml::sax::parser::
+} // end of namespace xml::sax::
+} // end of namespace xml::
+
+#endif
